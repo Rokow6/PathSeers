1e13d9c8bd8d4e096f46ee45606cc78e3d47aeda
Riley <rileydconnors@gmail.com>
2024-10-29 17:13:26 -0500

    Added AdminData table and related functions

--- app/src/main/java/edu/utsa/cs3773/pathseer/data/AppDatabase.java
+++ app/src/main/java/edu/utsa/cs3773/pathseer/data/AppDatabase.java
@@ -4,7 +4,7 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 // Database class used to access the database in code as an instanced object
-@Database(entities = {UserData.class, NotificationData.class, AdminData.class}, version = 3)
+@Database(entities = {UserData.class, NotificationData.class, AdminData.class}, version = 4)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao userDao();
     public abstract NotificationDao notificationDao();

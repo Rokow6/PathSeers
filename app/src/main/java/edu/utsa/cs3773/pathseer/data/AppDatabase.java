58288e5f1186d1aa3cf3adc3124c7b95a39ce870
Riley <rileydconnors@gmail.com>
2024-10-30 21:18:00 -0500

    Added JobSeekerData table

--- app/src/main/java/edu/utsa/cs3773/pathseer/data/AppDatabase.java
+++ app/src/main/java/edu/utsa/cs3773/pathseer/data/AppDatabase.java
@@ -4,9 +4,10 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 // Database class used to access the database in code as an instanced object
-@Database(entities = {UserData.class, NotificationData.class, AdminData.class}, version = 4)
+@Database(entities = {UserData.class, NotificationData.class, AdminData.class, JobSeekerData.class}, version = 5)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao userDao();
     public abstract NotificationDao notificationDao();
     public abstract AdminDao adminDao();
+    public abstract JobSeekerDao jobSeekerDao();
 }

e135058dc4bd2e1a7b4b828e4e76e70bb5413d7b
Riley <rileydconnors@gmail.com>
2024-10-31 12:00:56 -0500

    Added Salt string to UserData

--- app/src/main/java/edu/utsa/cs3773/pathseer/data/UserDao.java
+++ app/src/main/java/edu/utsa/cs3773/pathseer/data/UserDao.java
@@ -22,8 +22,9 @@ public interface UserDao {
 
     // Adds a new user to the database (id is auto-incremented)
     // Make sure to check the database with getUserIDFromUsername to check if the username already exists
-    @Query("INSERT INTO UserData (age,name,bio,username,password) VALUES (:age,:name,:bio,:username,:password)")
-    void addUserData(int age, String name, String bio, String username, String password);
+    @Query("INSERT INTO UserData (age,name,bio,username,password,salt) VALUES (:age,:name,:bio,:username,:password,:salt)")
+    void addUserData(int age, String name, String bio, String username, String password, String salt);
+
 
     // Deletes a user from the database
     @Query("DELETE FROM UserData WHERE userID = (:userID)")
@@ -33,6 +34,6 @@ public interface UserDao {
     // Make sure if updating username to check getUserIDFromUsername to see if the username already exists
     // NOTE: updates are slow and may take a while before the new data can be retrieved;
     // we could consider using some sort of cache to access recently used data
-    @Query("UPDATE UserData SET age = :age, name = :name, bio = :bio, username = :username, password = :password WHERE userID = (:userID)")
-    void updateUserData(int userID, int age, String name, String bio, String username, String password);
+    @Query("UPDATE UserData SET age = :age, name = :name, bio = :bio, username = :username, password = :password, salt = :salt WHERE userID = (:userID)")
+    void updateUserData(int userID, int age, String name, String bio, String username, String password, String salt);
 }
